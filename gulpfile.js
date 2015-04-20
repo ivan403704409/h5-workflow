e575af8fc1d305390e0f5e6f2b9ed5dfef6e9d4a
ivan403704409 <403704409@qq.com>
2015-04-20 13:19:28 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,36 @@
+/**
+ * Author: Ivan
+ * update: 2015.04.20
+ */
 var gulp = require('gulp');
-var	plugins = require('gulp-load-plugins'),
-	P = plugins({
-		rename: {
-			// 'gulp-minify-css': 'cssmin'
-		}
-	});	//获取插件集合
+var plugins = require('gulp-load-plugins'),
+  P = plugins({
+    rename: {
+      // 'gulp-minify-css': 'cssmin'
+    }
+  }); //获取插件集合
 
 //配置参数
 var path = {
-	src: 'src/',
-	dest: 'dist/'
+  src: 'src/',
+  dest: 'dist/'
 };
 
-
-///
 //清空dist
 gulp.task('clean-dist-all', function (){
   return gulp.src(path.dest)
     .pipe(P.clean());
 });
 
-//copy到dist,copy之前要清空
-gulp.task('copy-all', ['clean-dist-all'], function () {
+//less编译,错误会报错
+gulp.task('less-src', function(){
+  gulp.src( path.src + '**/*.less' )   //拿到源文件
+    .pipe(P.less())
+    .pipe(gulp.dest( path.src ));      //输出
+});
+
+//copy到dist,copy之前要清空和less编译检查
+gulp.task('copy-all', ['clean-dist-all', 'less-src'], function () {
   return gulp.src(path.src + '**/*')
     .pipe(gulp.dest(path.dest));
 }); 
@@ -32,33 +41,29 @@ gulp.task('clean-dist-less', ['copy-all'],  function (){
     .pipe(P.clean());
 });
 
-// gulp.task('cssmin', ['copy-all'], function() {
-//   gulp.src( path.dest + '**/*.css' )    //拿到源文件
-//     .pipe(P.minifyCss())         //压缩
-//     // .pipe(P.rename({ extname: '.css' }))
-//     .pipe(gulp.dest( path.dest ));     //输出
-// });
 
-////
 
-//less编译
-gulp.task('less-src', function(){
+//开发时实时编译---------------------------------------------------
+
+//less编译,忽略错误
+gulp.task('less-src-ignoreError', function(){
   gulp.src( path.src + '**/*.less' )   //拿到源文件
+    .pipe(P.plumber())                 //plumber给pipe打补丁,忽略错误,继续执行
     .pipe(P.less())
     .pipe(gulp.dest( path.src ));      //输出
 });
 
-//监听less
+//监听less,忽略错误
 gulp.task('watch-less', function() {
-  gulp.watch(path.src + '**/*.less', ['less-src']);
+  gulp.watch(path.src + '**/*.less', ['less-src-ignoreError']);
 });
 
+//开发时实时编译end---------------------------------------------------
 
-//编译less
-gulp.task('build-less', ['less-src']);
 
-//发布
-gulp.task('build-dist', ['clean-dist-less']);
+
+//发布,清空-->less编译检查-->复制-->删除.less
+gulp.task('build', ['clean-dist-less']);
 
 
 
