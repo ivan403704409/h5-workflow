45b26ba80a40c1e4b720345d810d2f19c05d6130
ivan403704409 <403704409@qq.com>
2015-05-11 14:20:54 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 /**
  * Author: Ivan
  * create: 2015.04.20
- * update: 2015.05.08
+ * update: 2015.05.11
  */
 var gulp = require('gulp');
+var pngquant = require('imagemin-pngquant');  //处理png
+var jpegtran = require('imagemin-jpegtran');  //处理jpg
 var plugins = require('gulp-load-plugins'),
   P = plugins({
     rename: {
@@ -66,85 +68,85 @@ gulp.task('watch', function() {
 
 
 //---------------------------构建上线代码---------------------------------------------------
-//1.清空dist
-gulp.task('clean-dist-all', function (){
+//清空dist
+gulp.task('clean-dist', function (){
   return gulp.src(path.dest)
     .pipe(P.clean());
 });
 
-//2.sass编译,错误会报错
-gulp.task('sass', function(){
+//sass编译,错误会报错
+gulp.task('sass-src', function(){
   gulp.src( path.src + '**/*.scss' )   //拿到源文件
     .pipe(P.sass())
     .pipe(gulp.dest( path.src ));      //输出
 });
 
-//3.copy所有文件到dist,copy之前要清空和sass编译检查
-gulp.task('copy-all', ['clean-dist-all', 'sass'], function () {
+//copy所有文件到dist
+gulp.task('copy-src', function () {
   return gulp.src(path.src + '**/*')
     .pipe(gulp.dest(path.dest));
 }); 
 
-//4.清除dist中的sass
-gulp.task('clean-dist-sass', ['copy-all'],  function (){
-  gulp.src(path.dest + '**/*.scss')
+//清除dist中的sass
+gulp.task('cleanSass-dist',  function (){
+ return gulp.src(path.dest + '**/*.scss')
     .pipe(P.clean());
 });
 
-//5.压缩dist中的css
-gulp.task('cssmin', ['clean-dist-sass'], function(){
+//压缩dist中的css
+gulp.task('cssMin-dist', function(){
   gulp.src( path.dest + '**/*.css' )   //拿到源文件
     .pipe(P.minifyCss(/*{keepBreaks:true}*/))  
     .pipe(gulp.dest( path.dest ));      //输出
 });
 
-//6.压缩dist中的js
-gulp.task('jsmin', ['cssmin'], function(){
+//压缩dist中的js
+gulp.task('jsMin-dist', function(){
   gulp.src( path.dest + '**/*.js' )   //拿到源文件
-    .pipe(P.uglify())                 //压缩
+    .pipe(P.uglify())   
+//  .pipe(P.rename({suffix: '.min'}))  //重命名              //压缩
     .pipe(gulp.dest( path.dest ));    //输出
 });
+// //压缩图片
+gulp.task('imgMin-dist', function() {
+  gulp.src( path.dest + '**/*.jpg' )  //拿到源文件
+    .pipe(P.imagemin({
+      // progressive: true,
+      // interlaced: true,
+      use: [pngquant(), jpegtran()]   //压缩png,有的会自动转成png8
+    }))          //压缩
+    .pipe(gulp.dest( path.dest ));       //输出
+});
 
+var fileName = process.cwd().match(/\\([^\\]+)$/)[1] + '.zip';  //当前目录名
 //生成zip文件,并以所在目录名命名
 gulp.task('zip', function () {
-  var fileName = process.cwd().match(/\\([^\\]+)$/)[1] + '.zip';    //当前目录名
-  gulp.src( path.dest + '*')
+  gulp.src( path.dest + '**/*')
         .pipe(P.zip(fileName))    //压缩成.zip
         .pipe(gulp.dest('./'));
-})
-//构建发布用的代码,清空-->sass编译检查-->复制-->删除.sass-->压缩css
-gulp.task('build', ['jsmin']);
+});
+
+
+
+//构建发布用的代码,清空-->sass编译检查-->复制-->【删除.sass, 压缩css, 压缩js】
+gulp.task('build', P.sequence(  //处理执行顺序
+  'clean-dist',
+  'sass-src', 
+  'copy-src',
+  'cleanSass-dist', 
+  ['cssMin-dist', 'jsMin-dist', 'imgMin-dist'], 
+  'zip'
+));
 //---------------------------构建上线代码---------------------------------------------------
 
-  
 
 
+//任务提示
+gulp.task('default', function () {  
+  console.log('$$$');
+  console.log('$$$ gulp watch --> 开启实时编译');
+  console.log('$$$ gulp build --> 构建上线代码');
+  // console.log('$$$ gulp zip ----> 打包压缩成“'+ fileName +'”发给需求方');
+  console.log('$$$');
+});
 
-
-
-
-
-// //压缩js
-// gulp.task('jsmin', function() {
-//   gulp.src( path.src + '**/*.js' )   //拿到源文件
-//     .pipe(P.uglify())          //压缩
-//     .pipe(P.rename({suffix: '.min'}))  //重命名
-//     .pipe(gulp.dest( path.dest ));   //输出
-// });
-
-// //压缩css
-// gulp.task('cssmin', function() {
-//   gulp.src( path.src + '**/*.css' )    //拿到源文件
-//     .pipe(P.minifyCss())         //压缩
-//     .pipe(P.rename({ extname: '.css' }))
-//     .pipe(gulp.dest( path.dest ));     //输出
-// });
-
-// //压缩图片
-// gulp.task('imgmin', function() {
-//   gulp.src( path.src + '**/*' )  //拿到源文件
-//     .pipe(P.imagemin())          //压缩
-//     .pipe(gulp.dest( path.dest ));       //输出
-// });
-// 
-// 
